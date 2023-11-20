ee67e2fb2eaf856c48eaef9aeeac871ce6a6ff9d
Maximilian Fichtl <mfichtl@googlemail.com>
2023-11-20 18:12:51 -0300

    animations for replicator dynamics

---
+++ lyapunov/linearization.py
@@ -0,0 +1,114 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def create_B(n):
+    B = []
+    interval_length = 1./n
+    for i in range(n):
+        Bi = np.zeros(shape=(n,n),dtype=float)
+        # entries k < j < i
+        for j in range(i):
+            for k in range(j):
+                Bi[j][k] = interval_length**3
+        # entries j=j < i
+        for j in range(i):
+            Bi[j][j] = (interval_length**3)/2
+        #entries k < i
+        for k in range(i):
+            Bi[i][k] = (interval_length**3)/2
+        # entry ii
+        Bi[i][i] = (interval_length**3)/6
+        B.append(Bi)
+    return B
+
+def test_B_uniform(n):
+    interval_length = 1./n
+    f = np.ones(shape=(n,1))
+    g = np.ones(shape=(n,1))
+    results = [np.dot(g.transpose(),np.dot(B[i],f))[0][0] for i in range(n)]
+    goal = [(((i+1)*interval_length)**3-(i*interval_length)**3)/6 for i in range(n)]
+    plt.plot(results)
+    plt.plot(goal,'.')
+    plt.show()
+
+def create_C(n):
+    C = []
+    interval_length = 1./n
+    for i in range(n):
+        Ci = np.zeros(shape=(1,n),dtype=float)
+        for j in range(i):
+            Ci[0][j] = (interval_length**3)/2
+        bi = i*interval_length
+        bip1 = (i+1)*interval_length
+        for j in range(i):
+            Ci[0][j] = (bip1**2-bi**2)/2 * interval_length
+        Ci[0][i] = (bip1**3-bi**3)/3 - bi*(bip1**2-bi**2)/2
+        C.append(Ci)
+    return C
+
+
+def test_C_uniform(n):
+    interval_length = 1./n
+    f = np.ones(shape=(n,1))
+    g = np.ones(shape=(n,1))
+    results = [np.dot(C[i],g)[0][0] for i in range(n)]
+    goal = [(((i+1)*interval_length)**3-(i*interval_length)**3)/3 for i in range(n)]
+    plt.plot(results)
+    plt.plot(goal,'.')
+    plt.show()
+
+
+def get_Psi(n, B, C, f,g):
+    alpha = np.array([(np.dot(np.dot(g.transpose(),B[i]),f)-np.dot(C[i],g))[0][0] for i in range(n)])
+    beta = np.dot(np.diag(f.flatten()), alpha)
+    gamma = np.dot(np.dot(np.diag(f.flatten()/n), np.ones((n,n))),beta)
+    Dfalpha = np.zeros((n,n))
+    Dgalpha = np.zeros((n,n))
+    for i in range(n):
+        Dfalpha[i,:] = np.dot(g.transpose(), B[i])
+        Dgalpha[i,:] = np.dot(f.transpose(), B[i].transpose()) - C[i]
+    beta = np.dot(np.diag(f.flatten()), alpha)
+    Dfbeta = np.diag(alpha) + np.dot(np.diag(f.flatten()), Dfalpha)
+    Dgbeta = np.dot(np.diag(f.flatten()),Dgalpha)
+    gamma = np.dot(np.dot(np.diag(f.flatten()/n), np.ones((n,n))),beta)
+    Dfgamma = np.sum(beta)*np.diag([1/n for i in range(n)]) + np.dot(np.dot(np.diag(f.flatten()/n), np.ones((n,n))), Dfbeta)
+    Dggamma = np.dot(np.dot(np.diag(f.flatten()/n), np.ones((n,n))), Dgbeta)
+    Psi = n * (beta - gamma)
+    DfPsi = n * (Dfbeta - Dfgamma)
+    DgPsi = n * (Dgbeta - Dggamma)
+    DPsi = np.concatenate((DfPsi,DgPsi), axis = 1)
+    return Psi,DPsi
+
+class PsiCalculator:
+
+    def __init__(self, n) -> None:
+        self.n = n
+        self.B = create_B(n)
+        self.C = create_C(n)
+        # Dimension reduction
+        X = np.eye(n,n-1) # maps n-1 vectors to n vectors (linear part)
+        for i in range(n-1):
+            X[n-1,i] = -1.
+        self.DP = np.stack((X,X))
+
+    def computeAt(self, f, g):
+        Psif, DPsif = get_Psi(self.n,self.B,self.C,f,g)
+        Psig, DPsig = get_Psi(self.n,self.B,self.C,g,f)
+        DPsig = DPsig[:, list(range(self.n,2*self.n))+list(range(self.n))]
+        return {
+            'Psif': Psif,
+            'DPsif': DPsif,
+            'Psig': Psig,
+            'DPsig': DPsig
+        }
+    
+    def computeReducedAt(self, tilde_f, tilde_g):
+        f = np.concatenate((tilde_f, [[self.n - np.sum(tilde_f)]]))
+        g = np.concatenate((tilde_g, [[self.n - np.sum(tilde_g)]]))
+        Psi = self.computeAt(f,g)
+        return {
+            'Psif': Psi['Psif'][0:self.n-1],
+            'DPsif': None,
+            'Psig': Psi['Psig'][0:self.n-1],
+            'DPsig': None,
+        }
--- lyapunov/simulation.py
+++ lyapunov/simulation.py
@@ -2,9 +2,12 @@ import numpy as np
 from random import randint
 from matplotlib import pyplot as plt
 from tqdm import tqdm
+from linearization import create_B, create_C, get_Psi
 
 # number of steps of the function
-n = 10
+n = 2
+
+edges = np.linspace(start = 0.,stop = 1., num=n+1)
 
 # subdivide the interval in
 interval_length = 1./n
@@ -13,85 +16,14 @@ interval_length = 1./n
 f = np.ones(shape=(n,1))
 g = np.ones(shape=(n,1))
 
-# Vector of the matrices B^i
-B = []
 
-for i in range(n):
-    Bi = np.zeros(shape=(n,n),dtype=float)
-    # entries k < j < i
-    for j in range(i):
-        for k in range(j):
-            Bi[j][k] = interval_length**3
-    # entries j=j < i
-    for j in range(i):
-        Bi[j][j] = (interval_length**3)/2
-    #entries k < i
-    for k in range(i):
-        Bi[i][k] = (interval_length**3)/2
-    # entry ii
-    Bi[i][i] = (interval_length**3)/6
-    B.append(Bi)
+B = create_B(n)
 
-def test_B_uniform():
-    f = np.ones(shape=(n,1))
-    g = np.ones(shape=(n,1))
-    results = [np.dot(g.transpose(),np.dot(B[i],f))[0][0] for i in range(n)]
-    goal = [(((i+1)*interval_length)**3-(i*interval_length)**3)/6 for i in range(n)]
-    plt.plot(results)
-    plt.plot(goal,'.')
-    plt.show()
-
-#test_B_uniform()
-#exit(0)
-
-# Vector of row vectors C^i
-C = []
-for i in range(n):
-    Ci = np.zeros(shape=(1,n),dtype=float)
-    for j in range(i):
-        Ci[0][j] = (interval_length**3)/2
-    bi = i*interval_length
-    bip1 = (i+1)*interval_length
-    for j in range(i):
-        Ci[0][j] = (bip1**2-bi**2)/2 * interval_length
-    Ci[0][i] = (bip1**3-bi**3)/3 - bi*(bip1**2-bi**2)/2
-    C.append(Ci)
-
-
-def test_C_uniform():
-    f = np.ones(shape=(n,1))
-    g = np.ones(shape=(n,1))
-    results = [np.dot(C[i],g)[0][0] for i in range(n)]
-    goal = [(((i+1)*interval_length)**3-(i*interval_length)**3)/3 for i in range(n)]
-    plt.plot(results)
-    plt.plot(goal,'.')
-    plt.show()
-
-#test_C_uniform()
-#exit(0)
+C = create_C(n)
 
 
 
-def get_Psi(f,g):
-    alpha = np.array([(np.dot(np.dot(g.transpose(),B[i]),f)-np.dot(C[i],g))[0][0] for i in range(n)])
-    beta = np.dot(np.diag(f.flatten()), alpha)
-    gamma = np.dot(np.dot(np.diag(f.flatten()/n), np.ones((n,n))),beta)
-    Dfalpha = np.zeros((n,n))
-    Dgalpha = np.zeros((n,n))
-    for i in range(n):
-        Dfalpha[i,:] = np.dot(g.transpose(), B[i])
-        Dgalpha[i,:] = np.dot(f.transpose(), B[i].transpose()) - C[i]
-    beta = np.dot(np.diag(f.flatten()), alpha)
-    Dfbeta = np.diag(alpha) + np.dot(np.diag(f.flatten()), Dfalpha)
-    Dgbeta = np.dot(np.diag(f.flatten()),Dgalpha)
-    gamma = np.dot(np.dot(np.diag(f.flatten()/n), np.ones((n,n))),beta)
-    Dfgamma = np.sum(beta)*np.diag([1/n for i in range(n)]) + np.dot(np.dot(np.diag(f.flatten()/n), np.ones((n,n))), Dfbeta)
-    Dggamma = np.dot(np.dot(np.diag(f.flatten()/n), np.ones((n,n))), Dgbeta)
-    Psi = n * (beta - gamma)
-    DfPsi = n * (Dfbeta - Dfgamma)
-    DgPsi = n * (Dgbeta - Dggamma)
-    DPsi = np.concatenate((DfPsi,DgPsi), axis = 1)
-    return Psi,DPsi
+
 
 
 #h = .01
@@ -107,23 +39,27 @@ def get_Psi(f,g):
 #plt.show()
 #exit(0)
 
+
+
 f = 0.1 * np.ones((n,1))
 f[0:1,0] += np.ones((1,))
 f = f * n/f.sum()
 g = 0.1 * np.ones((n,1))
 g[0:1,0] = np.ones((1,))
 g = g * n/g.sum()
-n_rounds = 1000
+n_rounds = 10000
 h = 0.1
 gradient_norms = []
 for k in tqdm(range(n_rounds)):
-    Psif, _ = get_Psi(f,g)
-    Psig, _ = get_Psi(g,f)
+    Psif, _ = get_Psi(n,B,C,f,g)
+    Psig, _ = get_Psi(n,B,C,g,f)
     gradient_norms.append(np.linalg.norm(Psif))
     f[:,0] += h*Psif
     g[:,0] += h*Psig
 
 print(np.sum(f)/n)
 #plt.plot(gradient_norms)
-plt.plot(f)
+plt.stairs(values=f.flatten(), edges=edges)
 plt.show()
+
+
